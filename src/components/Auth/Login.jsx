419621b8f06fa1b12fc820bf014133fc1a9d5f8a
Sumit Patidar <sumitpatidar12342@example.com>
2025-11-13 22:33:12 +0530

    First commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () =>{
+    return (
+        <div className="flex justify-center items-center h-screen w-screen bg-gray-500">
+Login Page
+        </div>
+    )
+}
+
+export default Login;
